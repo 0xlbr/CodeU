f36aff97fc9a501e693a310962644e142f3dc6dc
Lambrina <lambrina.kostova@gmail.com>
2017-06-22 02:44:12 +0100

    fix style

--- assignment2/PrintAncestors.java
+++ assignment2/PrintAncestors.java
@@ -1,11 +1,11 @@
 public class PrintAncestors {
-        /**
+	/**
 	* This method is used to print the ancestors of a node with a given key in a tree starting from the lowest one.
 	* @param tree This is the tree.
 	* @param key This is the key that is being searched for.
 	* @return boolean This is true if the key is found in the tree and false otherwise.
 	*/
-	public static <T> boolean ancestors(Tree<T> tree, T key){
+	public static <T> boolean ancestors(Tree<T> tree, T key) {
 		if (tree == null) {
 			return false;
 		} else if (tree.key == key) {
